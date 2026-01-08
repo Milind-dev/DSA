9ff32a16ffe4e7e6ac9d2e2804e81a4fa383f182
Milind-dev <milinddev1011@gmail.com>
2026-01-09 01:59:01 +0530

    remove duplicate

---
+++ dsa/Strings/removeduplicates.js
@@ -0,0 +1,18 @@
+
+function loader(arr){
+    let freq = {};
+    let result = "";
+    for (const ch of arr) {
+        if(!freq[ch]){
+            // freq[ch] = true;
+            console.log(!freq[ch],ch,freq[ch]);
+            
+            result  += ch;
+        }
+    }
+    return result
+}
+
+const arr = "abcced"
+
+console.log(loader(arr));
