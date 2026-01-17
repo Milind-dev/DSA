9eab53fe96c8da9f87bc1bd049b0b1f9a9669286
Milind-dev <milinddev1011@gmail.com>
2026-01-18 01:37:19 +0530

    reduce highest number

--- dsa/Random/practice/arrays.js
+++ dsa/Random/practice/arrays.js
@@ -1,16 +1,19 @@
 
 function loader(input) {
-    let arr = input.split("").map(Number);
-    let maxelement = arr[0]
-
-  // Frequency count
-  for (let ch of arr) {
-    if (ch > maxelement) {
-      maxelement = ch;
-    }
-  }
+    return input.split("").map(Number).reduce((max,curr) => {
+        return curr > max ? curr:max;
+    })
+    
+//     let arr = input.split("").map(Number);
+//     let maxelement = arr[0]
+//   // Frequency count
+//   for (let ch of arr) {
+//     if (ch > maxelement) {
+//       maxelement = ch;
+//     }
+//   }
   
-  return maxelement;
+//   return maxelement;
 }
-const input = "173207032";
+const input = "1732070392";
 console.log(loader(input));
