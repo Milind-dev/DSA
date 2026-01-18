48eb0502efcc9736f65249d33f76bf4bb73472f1
Milind-dev <milinddev1011@gmail.com>
2026-01-18 16:19:54 +0530

    arryas

--- dsa/Random/practice/arrays.js
+++ dsa/Random/practice/arrays.js
@@ -1,23 +1,10 @@
-//alternative subarryys
+//a3b2c1;
 
 function loader(arr) {
-  let left = 0;
-  let right = 1;
-  let maxlen = 1;
+   
 
-  while(right < arr.length){
-    if(arr[right] !== arr[right-1]){
-      maxlen = Math.max(maxlen,right - left+1);
-    }
-    else{
-      //now update left when condition fully above when its equal
-      left = right; //index
-    }
-    right++;
-  }
-  return maxlen;
 
 }
 
 
-console.log(loader([1,0,1,0,1,1,0,0,1])); // 5
+console.log(loader("ababcab")); // 3
