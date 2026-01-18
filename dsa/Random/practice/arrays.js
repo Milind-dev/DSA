ad5612a00dee8a712584df8d90eab884c3fca774
Milind-dev <milinddev1011@gmail.com>
2026-01-18 18:29:37 +0530

    max min element

--- dsa/Random/practice/arrays.js
+++ dsa/Random/practice/arrays.js
@@ -1,12 +1,31 @@
-//moves zeos
-function moveZeros(arr) {
-  let result = [];
-  let zeros = [];
+function loader(arr) {
+  let left = 0;
+  let right = 1;
 
-  for (let n of arr) {
-    n === 0 ? zeros.push(0) : result.push(n);
+  let min = arr[0];
+  let max = arr[0];
+
+  while (right < arr.length) {
+    // adjacent comparison
+    if (arr[left] > arr[right]) {
+      if (arr[right] < min) min = arr[right];
+      if (arr[left] > max) max = arr[left];
+    } else if (arr[left] < arr[right]) {
+      if (arr[left] < min) min = arr[left];
+      if (arr[right] > max) max = arr[right];
+    } else {
+      // equal case
+      if (arr[left] < min) min = arr[left];
+      if (arr[left] > max) max = arr[left];
+    }
+
+    // move window
+    left++;
+    right++;
   }
-  return result.concat(zeros);
+
+  return { min, max };
 }
 
-console.log(loader([1, 2, 0, 1, 3, 0])); // 3
+console.log(loader([1, 2, 0, 1, 3, 0]));
+// { min: 0, max: 3 }
