3a77bcacc883427aeccf9330942c74ac5b24e375
Milind-dev <milinddev1011@gmail.com>
2026-01-18 17:44:01 +0530

    move zeros

--- dsa/Random/practice/arrays.js
+++ dsa/Random/practice/arrays.js
@@ -1,10 +1,18 @@
-//a3b2c1;
-
+//moves zeros
 function loader(arr) {
-   
-
-
+  let store = []
+  let zeros = []
+  let result = []
+  for(let ch of arr){
+    if(ch ===0){
+      zeros.push(ch);
+    }
+    else{
+      result.push(ch);
+    }
+  }
+  return result.concat(zeros)
 }
 
 
-console.log(loader("ababcab")); // 3
+console.log(loader([1,2,0,1,3,0])); // 3
