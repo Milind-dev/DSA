aa53e1409846626789332035564f17c363d7f400
Milind-dev <milinddev1011@gmail.com>
2026-01-18 13:29:59 +0530

    correct smallest pair

--- dsa/Random/practice/arrays.js
+++ dsa/Random/practice/arrays.js
@@ -1,11 +1,41 @@
+// // smallest pair
+// function loader(array) {
+//   let min1 = Infinity;
+//   let min2 = Infinity;
 
-function mul(a) {
-  return function (b) {
-    return function (c) {
-      return a * b * c;
-    };
-  };
+//   for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     if (min1 > element) {
+//       min2 = min1;
+//       min1 = min1;
+//     } else {
+//       min2 = array[index];
+//     }
+//   }
+//   return min1 + min2;
+
+//   // console.log(min1, min2);
+// }
+
+// console.log(loader([3, 2, 3, 4, 5, 6]));
+
+// smallest pair
+function loader(array) {
+  let min1 = Infinity;
+  let min2 = Infinity;
+
+  for (let ch of array) {
+    const element = array[ch]; 3
+    if (min1 > element) { 3 > 9
+      min2 = min1;   min2 = inf
+      min1 = ch;    min1 = 3
+    } else {
+      min2 = ch;  min2 = 9
+    }
+  }
+  console.log(min1, min2);
+  return min1 + min2;
+
 }
 
-// const mul = mul(input)
-console.log(mul(1)(2)(3));
+console.log(loader([3, 2, 3, 4, 5, 6]));
